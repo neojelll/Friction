4b4041a4f22915244e9fb5b7c9960be7c80db202
neojelll <timofeymit.work@gmail.com>
2026-04-13 02:09:17 +0300

    fix: add item view on ground and local

--- prototypes/ores/tin/item.lua
+++ prototypes/ores/tin/item.lua
@@ -6,5 +6,8 @@ tin_item.order = "z[tin-ore]"
 
 tin_item.icon = "__Friction__/graphics/icons/ores/tin-ore.png"
 tin_item.icon_size = 64
+quartz_item.pictures = {
+  { size = 64, filename = "__Friction__/graphics/icons/tin-ore.png", scale = 0.7, mipmap_count = 1 },
+}
 
 data:extend({ tin_item })
