6b893951ff3a518dbf9376e96409d6b959aa6f41
neojelll <timofeymit.work@gmail.com>
2026-04-13 02:17:51 +0300

    fix: paths

--- prototypes/ores/tin/item.lua
+++ prototypes/ores/tin/item.lua
@@ -7,7 +7,7 @@ tin_item.order = "z[tin-ore]"
 tin_item.icon = "__Friction__/graphics/icons/ores/tin-ore.png"
 tin_item.icon_size = 64
 tin_item.pictures = {
-  { size = 64, filename = "__Friction__/graphics/icons/tin-ore.png", scale = 0.7, mipmap_count = 1 },
+  { size = 64, filename = "__Friction__/graphics/icons/ores/tin-ore.png", scale = 0.7, mipmap_count = 1 },
 }
 
 data:extend({ tin_item })
