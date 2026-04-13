9608034e62958ca68c0c3d6f9218ecb9fae97b74
Timofey <155233969+neojelll@users.noreply.github.com>
2026-04-13 20:48:20 +0300

    feat: other ores (#16)
    Closes #8 , #9 , #10

--- prototypes/ores/data.lua
+++ prototypes/ores/data.lua
@@ -19,6 +19,27 @@ local ores = {
     color = { r = 0.78, g = 0.65, b = 0.20 },
     order = "z[gold-ore]",
   },
+
+  {
+    name = "nickel-ore",
+    icon = "__Friction__/graphics/icons/ores/nickel-ore.png",
+    color = { r = 0.55, g = 0.45, b = 0.25 },
+    order = "z[nickel-ore]",
+  },
+
+  {
+    name = "lead-ore",
+    icon = "__Friction__/graphics/icons/ores/lead-ore.png",
+    color = { r = 0.42, g = 0.42, b = 0.42 },
+    order = "z[lead-ore]",
+  },
+
+  {
+    name = "zinc-ore",
+    icon = "__Friction__/graphics/icons/ores/zinc-ore.png",
+    color = { r = 0.29, g = 0.21, b = 0.13 },
+    order = "z[zinc-ore]",
+  },
 }
 
 return ores
