f1e22205a6e58db7cd8f370fab6abef1f3b44846
neojelll <timofeymit.work@gmail.com>
2026-04-19 12:07:59 +0300

    feat(crusher): add coal-powered crusher machine and crushing recipe category
    - new recipe-category 'crushing' (replaces handcraft for all crushing recipes)
    - crusher entity (furnace type, burner 90kW, 1x1 tile placeholder sprite)
    - crusher item + crafting recipe (8 iron-plate + 10 stone)
    - crushed-iron-ore item and crushing recipe (category: crushing)
    - crushed-quartz-ore recipe updated to category: crushing
    - iron-plate recipes: base slowed to 2 ore/6s, fast alt from crushed ore 2/3s
    - placeholder sprites from base game stone-furnace (TODO: replace)
    - locale en + ru for all new prototypes

---
+++ prototypes/buildings/crusher/item.lua
@@ -0,0 +1,13 @@
+data:extend({
+  {
+    type = "item",
+    name = "crusher",
+    localised_name = { "item-name.crusher" },
+    icon = "__Friction__/graphics/icons/buildings/crusher.png",
+    icon_size = 64,
+    subgroup = "production-machine",
+    order = "a[crusher]",
+    stack_size = 10,
+    place_result = "crusher",
+  },
+})
---
+++ prototypes/items/crushed-ores/data.lua
@@ -0,0 +1,4 @@
+require("prototypes.items.crushed-ores.iron.item")
+require("prototypes.items.crushed-ores.iron.recipe")
+require("prototypes.items.crushed-ores.quartz.item")
+require("prototypes.items.crushed-ores.quartz.recipe")
