fa55efa777b9c285d014e8af7399660603962c54
neojelll <timofeymit.work@gmail.com>
2026-04-21 23:56:43 +0300

    assets: add dedicated icons for crushed-quartz-ore and quartz-sand

--- prototypes/items/crushed-ores/quartz/item.lua
+++ prototypes/items/crushed-ores/quartz/item.lua
@@ -3,7 +3,7 @@ data:extend({
     type = "item",
     name = "crushed-quartz-ore",
     localised_name = { "item-name.crushed-quartz-ore" },
-    icon = "__Friction__/graphics/icons/ores/quartz/quartz-ore.png",
+    icon = "__Friction__/graphics/icons/items/crushed-ores/crushed-quartz.png",
     icon_size = 64,
     subgroup = "raw-resource",
     order = "e[crushed-quartz-ore]",
--- prototypes/items/quartz-sand/item.lua
+++ prototypes/items/quartz-sand/item.lua
@@ -5,7 +5,7 @@ data:extend({
     localised_name = { "item-name.quartz-sand" },
     icons = {
       {
-        icon = "__Friction__/graphics/icons/ores/quartz/quartz-ore.png",
+        icon = "__Friction__/graphics/icons/items/quartz-sand.png",
         icon_size = 64,
         tint = { r = 0.85, g = 0.78, b = 0.55, a = 1.0 },
       },
