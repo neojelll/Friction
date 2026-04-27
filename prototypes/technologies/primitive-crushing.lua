6e3da102f5a9a3e5edb73e70ad9d5cda01ea8a58
neojelll <timofeymit.work@gmail.com>
2026-04-27 19:12:32 +0300

    fix(research): remove hidden flag so technology is visible in tech tree

--- prototypes/technologies/primitive-crushing.lua
+++ prototypes/technologies/primitive-crushing.lua
@@ -5,7 +5,6 @@ data:extend({
     icon = "__Friction__/graphics/technology/primitive-crushing.png",
     icon_size = 256,
     enabled = true,
-    hidden = true,
     research_trigger = {
       type = "craft-item",
       item = "friction-research-token",
