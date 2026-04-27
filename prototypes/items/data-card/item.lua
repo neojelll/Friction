f2c1dc10067b5a58f6aba9fe615659c49761ba19
neojelll <timofeymit.work@gmail.com>
2026-04-28 00:35:39 +0300

    fix(data-card): use item-with-tags type to enable stack.tags support

--- prototypes/items/data-card/item.lua
+++ prototypes/items/data-card/item.lua
@@ -1,6 +1,6 @@
 data:extend({
   {
-    type = "item",
+    type = "item-with-tags",
     name = "friction-data-card",
     localised_name = { "item-name.friction-data-card" },
     icon = "__Friction__/graphics/icons/items/data-card.png",
@@ -8,6 +8,5 @@ data:extend({
     subgroup = "intermediate-product",
     order = "z[data-card]",
     stack_size = 1,
-    has_data = true,
   },
 })
